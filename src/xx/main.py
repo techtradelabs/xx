e4f51aaeb6816fc09c7104d6e36bc3fe3aace12f
tech149 <tech149@techtradelabs.com>
2020-01-03 07:23:18 +0000

    init

---
+++ src/xx/main.py
@@ -0,0 +1,45 @@
+
+
+
+class BaseRobot:
+    pass
+
+
+class Robot:
+    def __init__():
+        pass
+
+
+
+
+
+
+
+def main():
+
+
+    # load file
+
+    # replay file
+
+      # get tick
+      # decode
+      # md_reply
+      # look for subscription
+      # call on_md_event()
+      # also, on_timer()
+
+
+    # engine
+
+    #  create_robot("MSFT")
+
+    #  -> call md_svc.subscribe()
+
+    pass
+
+
+
+
+if __name__ == "__main__":
+    main()
